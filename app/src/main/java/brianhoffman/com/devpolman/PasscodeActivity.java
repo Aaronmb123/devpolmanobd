fa0170afa4174e7aab4617d964eac530d936bc66
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-12-03 10:46:23 -0800

    basic passcode activity working

--- app/src/main/java/brianhoffman/com/devpolman/PasscodeActivity.java
+++ app/src/main/java/brianhoffman/com/devpolman/PasscodeActivity.java
@@ -1,9 +1,39 @@
 package brianhoffman.com.devpolman;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PasscodeActivity extends AppCompatActivity {
 
+    private EditText mEnterPasscodeET;
+    private Button mPasscodeBTN;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passcode);
 
+        mEnterPasscodeET = (EditText) findViewById(R.id.passcode_et);
+        mPasscodeBTN = (Button) findViewById(R.id.passcode_btn);
+        mPasscodeBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEnterPasscodeET.getText().toString().equals("123456")) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    mEnterPasscodeET.setText("");
+                    Toast.makeText(getApplicationContext(),"Passcode incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
 }
