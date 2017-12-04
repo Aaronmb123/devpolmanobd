e211310a8ae732b1bf0218cb5df22e1734698c28
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-12-03 18:35:31 -0800

    wired button to start obd query service

--- app/src/main/java/brianhoffman/com/devpolman/PhoneLockerActivity.java
+++ app/src/main/java/brianhoffman/com/devpolman/PhoneLockerActivity.java
@@ -10,13 +10,16 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class PhoneLockerActivity extends AppCompatActivity {
 
-    Button mButtonEnable;
+    private static final String TAG = "PhoneLockerActivity";
+
+    private Button mButtonEnable;
     private Button mStartDriveSafeBtn;
     private Button mStopDriveSafeBtn;
     private Button mCloseBTN;
@@ -32,9 +35,19 @@ public class PhoneLockerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_locker);
 
-        // start ObdQueryService
-        Intent intent = ObdQueryService.newIntent(this);
-        getApplicationContext().startService(intent);
+        mStartDriveSafeBtn = (Button) findViewById(R.id.start_obd_service_btn);
+        mStartDriveSafeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start ObdQueryService
+//                Intent intent = ObdQueryService.newIntent(getApplicationContext());
+//                getApplicationContext().startService(intent);
+                ObdQueryService.setServiceAlarm(getApplicationContext());
+                Log.i(TAG, "ObdQueryService Started");
+
+            }
+        });
+
 
 
         // Retrieve a PendingIntent that will perform a broadcast
