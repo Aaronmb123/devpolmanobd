b6dfd2d3eae7646d9c590f1619eb71a2a2647013
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-12-03 19:49:10 -0800

    added query shared preference class

---
+++ app/src/main/java/brianhoffman/com/devpolman/QueryPreferences.java
@@ -0,0 +1,22 @@
+package brianhoffman.com.devpolman;
+
+import android.content.Context;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+
+public class QueryPreferences {
+
+    private static final String PREF_IS_ALARM_ON = "isAlarmOn";
+
+    public static boolean isAlarmOn(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(PREF_IS_ALARM_ON, false);
+    }
+
+    public static void setAlarmOn(Context context, boolean isOn) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putBoolean(PREF_IS_ALARM_ON, isOn)
+                .apply();
+    }
+
+}
