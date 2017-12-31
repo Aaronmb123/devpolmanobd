67e1f7db4f5fb0be08b7581feeb4302b7f0d66f5
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-12-31 13:00:09 -0800

    added interrupted flag to close button

--- app/src/main/java/brianhoffman/com/devpolman/EnableDriveSafeFragment.java
+++ app/src/main/java/brianhoffman/com/devpolman/EnableDriveSafeFragment.java
@@ -88,6 +88,7 @@ public class EnableDriveSafeFragment extends Fragment {
             if(android.os.Build.VERSION.SDK_INT >= 21) {
                 mActivity.finishAndRemoveTask();
             } else {
+                QueryPreferences.setInterrupted(mActivity,true);
                 mActivity.finish();
             }
 
